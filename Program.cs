140876a369455fd10406887e74eb9d27c47da801
Simon Dib <simon.dib@axa.fr>
2017-03-06 17:21:19 +0100

    hello world app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace netcorelive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !");
+            Console.ReadKey();
+        }
+    }
+}
