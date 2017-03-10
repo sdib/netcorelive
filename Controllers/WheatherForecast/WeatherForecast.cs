52f16ba9792d27bfa10192cc451a91cc11071c21
CHERVET Guillaume <guillaume.chervet@gmail.com>
2017-03-10 18:22:06 +0100

    maj

--- Controllers/WheatherForecast/WeatherForecast.cs
+++ Controllers/WheatherForecast/WeatherForecast.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace netcorelive.Controllers.WheatherForecast
 {
-        public class WeatherForecast
+    public class WeatherForecast
         {
             public string DateFormatted { get; set; }
             public int TemperatureC { get; set; }
